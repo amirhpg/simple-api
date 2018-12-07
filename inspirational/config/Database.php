9476f2778436bd9c1172213704e6daa9c2dfea7d
amirhpg <amirhpg.wm@gmail.com>
2018-12-07 16:34:42 +0330

    added some functionality to database file

--- inspirational/config/Database.php
+++ inspirational/config/Database.php
@@ -43,4 +43,65 @@ class Database{
             return $stmt->fetch();
 
     }
+
+    public function executeCall($username,$calls_allowed,$timeOutSeconds){
+        $query = "SELECT plan, calls_made, time_start, time_end FROM users WHERE username = '$username'";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute([$username]);
+        $result = $stmt->fetch();
+
+        //if it is timeout or eaual to zero set it to true
+        $timeOut = data(time()) - $result['time_start'] >= $timeOutSeconds || $result['time_start'] === 0;
+
+        //update calls made with respece to time out
+        $query = "UPDATE users SET calls_made = ";
+        $query .= $timeOut ? " 1, time_start = ".date(time())." , time_end = ".strtotime("+$timeOutSeconds seconds") : "calls_made + 1";
+        $query .= " WHERE username = ?";
+
+        //Insted of fetching again using select all update variables
+        $result['calls_made'] = $timeOut ? 1 : $result['calls_made'] + 1;
+        $result['time_end'] = $timeOut ? strtotime("+ $timeOutSeconds seconds") : $result['time_end'];
+
+        //execute code with respect to plans
+        if($result['plan'] == "unlimited"){
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute([$userName]);
+            return $result;
+        }else{
+            // if no time out and calls made is greateher than calls allowed return -1
+            if($timeOut == false && $result['calls_made'] >= calls_allowed){
+                return -1;
+            }else{
+                // grant access
+                $stmt = $this->pdo->prepare($query);
+                $stmt->execute([$username]);
+                return $result;
+            }
+        }
+
+    }
+
+    public function insertOne($query, $body, $user_id,$category_id,$date){
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute([$body,$category_id,$date,$id]);
+
+    }
+
+    public function updateOne($query,$body,$category_id,$date,$id){
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute([$body,$category_id,$date,$id]);
+    }
+
+    public function deleteOne($query,$id){
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute([$id]);
+    }
+
+    public function insertUser($query,$firstName,$lastName,$password,$username){
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute([$firstName,$lastName,$password,$username]);
+
+    }
+
+
 }
